11e150e30feab9f33220179b9c3093501fb104b3
MJ-Dev <majidbabaeifar@gmail.com>
2022-05-10 23:36:11 +0430

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,45 @@
 import React from "react";
-import { Container, Navbar } from "react-bootstrap";
+import { RiShoppingCartFill } from "react-icons/ri";
+import { Container, Dropdown, FormControl, Navbar, Nav, Badge } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <div>
-      <Navbar bg="dark" variant="dark" >
-        <Container >
-          <Navbar.Brand href="#home">
-            <Link to="/">React Bootstrap</Link>
+      <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+        <Container>
+          <Navbar.Brand>
+            <Link
+              className="text-decoration-none"
+              to="/"
+              style={{ color: "white" }}
+            >
+              Shopping Cart
+            </Link>
           </Navbar.Brand>
+          <Navbar.Text>
+            <FormControl
+              style={{ width: 500 }}
+              type="search"
+              placeholder="Search a product..."
+              className="m-auto"
+              aria-label="Search"
+            />
+          </Navbar.Text>
+          {/* <Nav> */}
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              <RiShoppingCartFill />
+              <Badge bg="secondary">9</Badge>
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          {/* </Nav> */}
         </Container>
       </Navbar>
     </div>
